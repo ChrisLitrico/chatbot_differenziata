a593cb69857a92a334124506efa988559137fbd1
ChrisLitrico <umamitas.twitch@gmail.com>
2026-02-25 01:01:39 +0100

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,21 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import './globals.css'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Differenziata in Sicilia",
-  description: "Guida alla raccolta differenziata in Sicilia.",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+  title: 'Differenziata in Sicilia',
+  description: 'Guida alla raccolta differenziata in Sicilia.',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
+}
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`bg-violet-200 pt-6 ${inter.className}`}>
-        {children}
-      </body>
+      <body className={`bg-violet-200 pt-6 ${inter.className}`}>{children}</body>
     </html>
-  );
+  )
 }
